8cfb2babe4da598ea77e6cf192850fc0a3111484
it21176456 <it21176456@my.sliit.lk>
2022-07-29 11:59:09 +0530

    create menu

--- ATM.c
+++ ATM.c
@@ -1,152 +1,59 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<stdbool.h>
 #include<math.h>
+#include<stdbool.h>
 
-void erromsg();
 void mainmenu();
-void menuexit();
-void checkbalance();
-float moneydeposit(float balance);
-float moneywithdraw(float balance);
-
-
+void erromsg();
 
 int main()
 {
-	int op,choose  ;
-	float balance = 15000.00 ;
-	bool again=true;
+	int option;
 	
+	bool again=true;
+
 	while(again)
 	{
 		mainmenu();
-		scanf("%d",&op);
-		
-		switch(op)
+		scanf("%d",&option);
+
+		switch(option)
 		{
 			case 1:
-			system("CLS");
-			void checkbalance();
-			break;
-			
-		case 2:
-			system("CLS");
-			balance=moneydeposit(balance);
-			break;
-		case 3:
-		    system("CLS");
-		    balance=moneywithdraw(balance);
-		    break;
-		case 4:
-			system("CLS");
-			menuexit();
-			return 0;
-		default :
-		    erromsg();
-		    break;
+			system("cls");
+				balance();
+				break;
+			case 2:
+			system("cls");
+				withdraw();
+				break;
+			case 3:
+			system("cls");
+				deposit();
+				break;
+			case 4:
+			system("cls");
+				menuexit();
+				return 0;
+			default:
+			    erromsg();
+				break;
 		}
-		
-	printf("would you like to do a another transaction??????\n");
-	printf("<1>yes\n");
-	printf("<2>no\n");
-	
-	scanf("%d",&choose);
-	system("CLS");
-	if(choose==2)
-	{
-	    again=false;
-	    menuexit();
 	}
-	
-	
-		        
-				
-		    
-	}
-	
-	return 0;
 }
+
 void mainmenu()
-{   
-    printf("====HELLO===\n");
-    printf("====GOOD MORNING====\n");
-    printf("$$$ SAMPATH ATM MACHINE $$$\n");
-	printf("main menu \n");
-	printf("<1>check balance\n");
-	printf("<2>deposit\n");
-	printf("<3>witdraw\n");
-	printf("<4>exit\n");
-}
-
-
-void checkbalance()
 {
-	float balance;
-    FILE*fpointer;
-    fpointer=fopen("balance.txt","r");
-
-    if(fpointer==NULL)
-    {
-        printf("file not found\n");
-    }
-   
-   fscanf(fpointer,"%f",balance);
-
-   printf("your balance is %.2f\n",balance);
-	
+	printf("============HELLO============\n");
+	printf("============GOOD MORNING============\n");
+	printf("============WELCOME TO SAMPATH ATM============\n");
+	printf("1. Check Balance\n");
+	printf("2. Withdraw\n");
+	printf("3. Deposit\n");
+	printf("4. Exit\n");
 }
-void menuexit()
-{   system("CLS");
-	printf("take your recipt\n");
-	printf("THANK YOU!\n");
-}
-float moneydeposit(float balance)
-{
-	float deposit;
-	printf("enter your amount= \n");
-	scanf("%f",&deposit);
-	printf("your deposited money is = %.2f\n",deposit);
-	
-	balance+=deposit;
-	printf("your new bank balance is = %.2f\n",balance);
-	
-	return balance;
-	
-}
-
-float moneywithdraw(float balance)
-{   
-float withdraw;
-    bool back=true;
-    while(back)
-    {   
-    		printf("enteryour amount= \n");
-	scanf("%f",&withdraw);
-	if(balance>withdraw)
-	{
-	printf("your withdrawal amount is = %.2f\n",withdraw);
-	
-	balance-=withdraw;
-	printf("your bank balance is = %.2f\n",balance);
-	
-	back=false;
-		
-	}
-	else 
-	{
-		printf("==you don't have enough money==\n");
-		printf("your bank balance is = %.2f \n",balance);
-	}
-	}
-
-	return balance;
-	
-}
-
-
 
 void erromsg()
 {
-	printf("++++++ you selected invalid number ++++++\n");
+	printf("Invalid Option\n");
 }
